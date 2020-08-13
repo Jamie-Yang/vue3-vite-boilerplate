8914c7ccf759989ce672c4f2c3d134cc3c876f87
JamieYang <jm.y.null@gmail.com>
2020-08-14 00:56:42 +0800

    feat(utils): 增加requestAnimFrame 封装

---
+++ src/utils/raf.ts
@@ -0,0 +1,21 @@
+/**
+ * Polyfill for requestAnimationFrame
+ * @borrows https://github.com/darius/requestAnimationFrame/blob/master/requestAnimationFrame.js
+ */
+let lastTime = 0
+function fallback(callback: (timestamp: number) => void) {
+  const now = Date.now()
+  const nextTime = Math.max(lastTime + 16, now)
+
+  return setTimeout(function () {
+    callback((lastTime = nextTime))
+  }, nextTime - now)
+}
+
+export function requestAnimFrame(callback: (timestamp: number) => void): number {
+  return (window.requestAnimationFrame || window.webkitRequestAnimationFrame || fallback)(callback)
+}
+
+export function cancelAnimFrame(id: number): void {
+  ;(window.cancelAnimationFrame || window.webkitCancelAnimationFrame || window.clearTimeout)(id)
+}
