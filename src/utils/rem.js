2b0abb6696bc8ea7fb3f50f18b90321042b21396
JamieYang <jm.y.null@gmail.com>
2020-08-30 22:26:57 +0800

    feat(utils/rem): 简化rem设置的无用代码

--- src/utils/rem.js
+++ src/utils/rem.js
@@ -1,4 +1,4 @@
-;(function flexible(window, document) {
+;(function remSetup(window, document) {
   const docEl = document.documentElement
   const isIOS = window.navigator.appVersion.match(/iphone|ipad|ipod|ios/gi)
   const isAndroid = window.navigator.userAgent.match(/Android[\S\s]+AppleWebkit\/(\d{3})/i)
@@ -6,36 +6,20 @@
 
   docEl.setAttribute('data-dpr', dpr)
 
-  // adjust body font size
-  function setBodyFontSize() {
-    if (document.body) {
-      document.body.style.fontSize = 12 * dpr + 'px'
-    } else {
-      document.addEventListener('DOMContentLoaded', setBodyFontSize)
-    }
-  }
-  setBodyFontSize()
-
   // set 1rem = 50px, 750px / 2x
-  function setRemUnit() {
-    docEl.style.fontSize = 50 * dpr + 'px'
-  }
-  setRemUnit()
+  docEl.style.fontSize = `${50 * dpr}px`
 
   // set viewport scale
-  function setViewportScale() {
-    const scale = 1 / dpr
+  const scale = 1 / dpr
 
-    let metaEl = document.querySelector('meta[name="viewport"]')
-    if (!metaEl) {
-      metaEl = document.createElement('meta')
-      metaEl.setAttribute('name', 'viewport')
-      document.head.appendChild(metaEl)
-    }
-    metaEl.setAttribute(
-      'content',
-      `width=device-width,user-scalable=no,initial-scale=${scale},maximum-scale=${scale},minimum-scale=${scale}`
-    )
+  let metaEl = document.querySelector('meta[name="viewport"]')
+  if (!metaEl) {
+    metaEl = document.createElement('meta')
+    metaEl.setAttribute('name', 'viewport')
+    document.head.appendChild(metaEl)
   }
-  setViewportScale()
+  metaEl.setAttribute(
+    'content',
+    `width=device-width,user-scalable=no,initial-scale=${scale},maximum-scale=${scale},minimum-scale=${scale}`
+  )
 })(window, document)
