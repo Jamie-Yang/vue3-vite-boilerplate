1e9f6aadd6a8ca5a0b72cae927fe4a9b3f37dcde
JamieYang <jm.y.null@gmail.com>
2021-05-23 18:54:36 +0800

    feat(api): 编写示例请求方法

--- src/api/module-name.ts
+++ src/api/module-name.ts
@@ -1,3 +1,21 @@
+import { request } from '@/utils/request'
+
 const { VITE_API_BASE_URL } = import.meta.env
 
-console.log(VITE_API_BASE_URL)
+interface IExample {
+  query1: string
+  query2: number
+}
+
+interface RExample {
+  data: string
+  list: string[]
+}
+
+export function fetchExample({ query1, query2 }: IExample): Promise<RExample> {
+  return request(`${VITE_API_BASE_URL}/path/to`, { query1, query2 })
+}
+
+fetchExample({ query1: '', query2: 1 }).then((res) => {
+  console.log(res.data, res.list)
+})
