f5f0dbf20f14e4277073da1c45e883b318a6cc33
JamieYang <jm.y.null@gmail.com>
2021-06-06 17:13:55 +0800

    feat(vuex): 尝试灵活自动导入子模块

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,14 @@
 import { createStore } from 'vuex'
+import type { ModuleTree } from 'vuex'
 
-import user from './modules/user'
+const modulesGlob = import.meta.globEager('./modules/*.ts')
+const modules = Object.keys(modulesGlob).reduce<ModuleTree<unknown>>(
+  (pre, k) => (pre[k] = modulesGlob[k].default),
+  {}
+)
 
 const store = createStore({
-  modules: { user },
+  modules,
   state: {},
   mutations: {},
   actions: {},
