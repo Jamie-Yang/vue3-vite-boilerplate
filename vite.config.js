3d93a8490745cfb94fc32dfbb2077625d507a891
JamieYang <jm.y.null@gmail.com>
2022-06-23 23:50:50 +0800

    chore(vite): 老浏览器环境构建

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import legacy from '@vitejs/plugin-legacy'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), legacy()],
+
+  css: {
+    postcss: {
+      plugins: [require('autoprefixer')],
+    },
+  },
 
   resolve: {
     alias: {
