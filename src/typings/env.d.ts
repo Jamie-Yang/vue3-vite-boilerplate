414b47350abf763c75c1362dc64fb0ca77836482
JamieYang <jm.y.null@gmail.com>
2023-06-09 23:55:35 +0800

    feat: 升级依赖，同步vue官方脚手架最新配置

--- src/typings/env.d.ts
+++ src/typings/env.d.ts
@@ -1,3 +1,5 @@
+/// <reference types="vite/client" />
+
 interface ImportMetaEnv {
   VITE_API_BASE_URL: string
 }
--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,14 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
 
-  css: {
-    postcss: {
-      plugins: [require('autoprefixer')],
-    },
-  },
-
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, '/src'),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
