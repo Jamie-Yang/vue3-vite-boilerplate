0d07154c1c98066d5a54a11d53d650c9b1e529b2
JamieYang <jm.y.null@gmail.com>
2023-10-09 15:38:43 +0800

    feat: 删除开发服务器 https

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import legacy from '@vitejs/plugin-legacy'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 
 export default defineConfig({
@@ -12,7 +11,6 @@ export default defineConfig({
   plugins: [
     vue(),
     legacy(),
-    basicSsl(),
     createSvgIconsPlugin({
       iconDirs: [fileURLToPath(new URL('./src/assets/icons', import.meta.url))],
       symbolId: 'icon-[dir]-[name]',
@@ -27,7 +25,6 @@ export default defineConfig({
 
   server: {
     host: true,
-    https: true,
     open: true,
     proxy: {
       // https://cn.vitejs.dev/config/server-options.html#server-proxy
