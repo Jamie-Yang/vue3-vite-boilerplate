33c300d9ca0265ce3eade846ffa2c1ac7912cee0
JamieYang <jm.y.null@gmail.com>
2023-10-09 15:30:49 +0800

    feat(vite): 增加svg图标使用、增加开发服务器 https

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,21 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import legacy from '@vitejs/plugin-legacy'
+import basicSsl from '@vitejs/plugin-basic-ssl'
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 
 export default defineConfig({
-  plugins: [vue(), legacy()],
+  base: '/',
+
+  plugins: [
+    vue(),
+    legacy(),
+    basicSsl(),
+    createSvgIconsPlugin({
+      iconDirs: [fileURLToPath(new URL('./src/assets/icons', import.meta.url))],
+      symbolId: 'icon-[dir]-[name]',
+    }),
+  ],
 
   resolve: {
     alias: {
@@ -15,6 +27,15 @@ export default defineConfig({
 
   server: {
     host: true,
+    https: true,
     open: true,
+    proxy: {
+      // https://cn.vitejs.dev/config/server-options.html#server-proxy
+      '/api': {
+        target: 'https://api.target.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
   },
 })
