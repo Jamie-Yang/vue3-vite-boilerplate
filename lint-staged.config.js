9cd3c4e862fdc0680a8737c33b3998287f652fd0
Jamie-Yang <jm.y.null@gmail.com>
2025-06-30 11:37:07 +0800

    fix(lint-staged): 修正配置

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 export default {
   '*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
-  '{!(package)*.json,*.code-snippets,.!(browserslist)*rc}': ['prettier --write--parser json'],
+  '{!(package)*.json,*.code-snippets,.!(browserslist)*rc}': ['prettier --write --parser json'],
   'package.json': ['prettier --write'],
   '*.vue': ['eslint --fix', 'prettier --write', 'stylelint --fix'],
   '*.{scss,css}': ['stylelint --fix', 'prettier --write'],
